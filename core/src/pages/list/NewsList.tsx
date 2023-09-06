b821e2b9d821575b1e790992033b809dfd84899d
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-09-06 20:51:02 +0330

    Create NewsList.tsx

---
+++ core/src/pages/list/NewsList.tsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import {} from "../../data/remote/dto/NewsListDto"
+
+interface ListProps {
+
+}
+
+export default function NewsList(props: ListProps) {
+    const [news, setNews]: NewsList = useState();
+
+}
